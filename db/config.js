a39719a91d7ad025d0e77e9c1f0b729b4bb56d8c
Vanita-Chawla49 <97331339+Vanita-Chawla49@users.noreply.github.com>
2025-03-12 21:48:43 +0500

    Add files via upload

--- db/config.js
+++ db/config.js
@@ -1,4 +1,4 @@
 const mongoose = require("mongoose")
 
-mongoose.connect("mongodb+srv://vinitachawla49:vinita12@cluster0.gpwvob3.mongodb.net/e-comm");
-// mongoose.connect("mongodb://localhost:27017/e-comm")
+mongoose.connect("mongodb+srv://vinitachawla49:vinita12@cluster0.0yy6x.mongodb.net/e-comm");
+// mongoose.connect("mongodb://localhost:27017/e-commerce")
